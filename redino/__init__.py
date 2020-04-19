79c86fdacf0e5c8c5b409183972400b575e7f82d
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-04-19 23:43:51 +0200

    Use the connection pool correctly

--- redino/__init__.py
+++ redino/__init__.py
@@ -1,12 +1,22 @@
 import redis
 
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, Optional
 import functools
 import logging
 
 LOG = logging.getLogger(__name__)
 
 T = TypeVar('T')
+_redis_instance: Optional[redis.StrictRedis] = None
+
+
+def _redis_pool() -> redis.StrictRedis:
+    global _redis_instance
+
+    if not _redis_instance:
+        _redis_instance = redis.StrictRedis()
+
+    return _redis_instance
 
 
 def transactional(f: Callable[..., T]) -> Callable[..., T]:
@@ -29,7 +39,7 @@ def transactional(f: Callable[..., T]) -> Callable[..., T]:
 def redis_connect(f: Callable[..., T]) -> Callable[..., T]:
     @functools.wraps(f)
     def wrapper(*args, **kw) -> T:
-        r = redis.StrictRedis()
-        return f(r, *args, **kw)
+        with _redis_pool().client() as redis_client:
+            return f(redis_client, *args, **kw)
 
     return wrapper
