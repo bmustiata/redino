ab5caa7effce2c7e1a32a8712e7aaab7d2bc275f
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-08-30 00:20:02 +0200

    mypy also passes.

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ packages = find_packages()
 
 setup(
     name="redino",
-    version="1.0.0",
+    version="0.1.master",
     description="redino",
     long_description=readme,
     author="Bogdan Mustiata",
     author_email="bogdan.mustiata@gmail.com",
     license="BSD",
-    install_requires=[],
+    install_requires=["redis==3.5.3"],
     packages=packages,
     package_data={
         "": ["*.txt", "*.rst"],
